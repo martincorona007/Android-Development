53dd815d8c85736f0c99b28fe8d54759784243b4
martin corona <martincorona007@hotmail.com>
2020-06-12 13:38:37 -0500

    adding database and view products

---
+++ subject/PV_sisto/app/src/main/java/com/example/pv_sisto/DBSQLite.java
@@ -0,0 +1,23 @@
+package com.example.pv_sisto;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBSQLite extends SQLiteOpenHelper {
+
+    public DBSQLite(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+        super(context,name,factory,version);
+    }
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("CREATE TABLE users(name text,email text,pwas text,typer text)");
+        db.execSQL("CREATE TABLE products(code text,name text,price text)");
+        db.execSQL("CREATE TABLE sales_a(ID_ch INTEGER PRIMARY KEY AUTOINCREMENT, subtot REAL,iv REAL,tota REAL)");
+        db.execSQL("CREATE TABLE sales_b(FK_ID_ch int,code int,FOREIGN KEY(FK_ID_ch) REFERENCES sales_a(ID_ch)); ");
+
+
+    }
+    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+
+    }
+}
---
+++ subject/PV_sisto/app/src/main/java/com/example/pv_sisto/view_Products_Activity.java
@@ -0,0 +1,14 @@
+package com.example.pv_sisto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class view_Products_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__products_);
+    }
+}
